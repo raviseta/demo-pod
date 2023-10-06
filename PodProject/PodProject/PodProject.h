09a39cd32bca3247cdd012fa8a670ac15297d36e
Ravi Seta <mr.raviseta@gmail.com>
2023-10-06 16:47:04 +0530

    first commit

---
+++ PodProject/PodProject/PodProject.h
@@ -0,0 +1,18 @@
+//
+//  PodProject.h
+//  PodProject
+//
+//  Created by Ravi Seta on 06/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodProject.
+FOUNDATION_EXPORT double PodProjectVersionNumber;
+
+//! Project version string for PodProject.
+FOUNDATION_EXPORT const unsigned char PodProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodProject/PublicHeader.h>
+
+
